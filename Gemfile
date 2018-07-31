c73d71b904c147ef0e4693dabb553cd4042ed2d6
Paul Hiatt <hiattp@gmail.com>
2018-07-31 15:28:23 -0600

    add active support for utilities (e.g. blank?)

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@
 
 source 'https://rubygems.org'
 
+gem 'activesupport'
+
 group :test do
   gem 'rspec'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'active_support/core_ext/object/blank'
 
 Dir[File.join(File.dirname(__FILE__), '..', 'transformers', '*.rb')].each do |file|
   require file
