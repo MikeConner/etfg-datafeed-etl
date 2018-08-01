bad51d5fb0022a7757cd3b0f68f5aff05414560f
birdylockstep <bird@locksteplabs.com>
2018-08-01 19:27:12 +0700

    Create etl job for bmo holdings

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,16 @@
-require 'csv'
 require 'active_support/core_ext/object/blank'
+require 'csv'
+require 'kiba'
+require 'yaml'
+
+Dir[File.join(File.dirname(__FILE__), '..', 'sources', '*.rb')].each do |file|
+  require file
+end
 
 Dir[File.join(File.dirname(__FILE__), '..', 'transformers', '*.rb')].each do |file|
+  require file
+end
+
+Dir[File.join(File.dirname(__FILE__), '..', 'destinations', '*.rb')].each do |file|
   require file
 end
---
+++ transformers/bmo_holdings_transformer.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class BmoHoldingsTransformer
+  def initialize(target_date:)
+    @target_date = target_date
+  end
+
+  def process(row)
+    new_row = {}
+    new_row[:ticker] = row['Ticker']
+    new_row[:instrument_type] = row['InstrumentType']
+    new_row[:name] = row['Name']
+    new_row[:sedol] = cleanup_data(row['SEDOL'])
+    new_row[:cusip] = cleanup_data(row['CUSIP'])
+    new_row[:security_id] = cleanup_data(row['SecurityID'])
+    new_row[:quantity_per_paramount] = row['Quantity/ParAmount']
+    new_row[:etfg_date] = Date.parse(@target_date)
+    new_row
+  end
+
+  private
+
+  def cleanup_data(data)
+    data.match?(/=\"(.*)\"/) ? data.match(/=\"(.*)\"/)[1] : data
+  end
+end
