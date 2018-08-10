f8a889cb40860772134f9d378df90b7e46d6da43
birdylockstep <bird@locksteplabs.com>
2018-08-10 16:31:06 +0700

    Use Kiba.run in spec instead

--- spec/transformers/bmo_holdings_transformer_spec.rb
+++ spec/transformers/bmo_holdings_transformer_spec.rb
@@ -1,39 +1,39 @@
 # frozen_string_literal: true
 
 RSpec.describe BmoHoldingsTransformer do
-  def get_data_from_csv(file)
-    rows = []
-    CSV.foreach(file, headers: true, liberal_parsing: true) do |row|
-      rows << row.to_hash
-    end
-    rows
-  end
-
   it 'transforms holdings files correctly' do
-    holdings_file_path = File.join(
+    holdings_filepath = File.join(
       File.dirname(__FILE__), '../fixtures/bmo/ETFDailyHoldings-20180725.test.csv'
     )
-    bmo_holdings = BmoHoldingsTransformer.new(target_date: '20180725')
-    rows = get_data_from_csv(holdings_file_path)
+    rows = []
+    control = Kiba.parse do
+      source CSVSource, holdings_filepath, headers: true, liberal_parsing: true
+      transform BmoHoldingsTransformer, target_date: '20180725'
+      transform do |row|
+        rows << row
+      end
+    end
 
-    first_new_row = bmo_holdings.process(rows.first)
-    expect(first_new_row[:ticker]).to eq 'BANK'
-    expect(first_new_row[:instrument_type]).to eq 'Equity'
-    expect(first_new_row[:name]).to eq 'AOZORA BANK LTD'
-    expect(first_new_row[:sedol]).to eq 'B1G1854'
-    expect(first_new_row[:cusip]).to be_blank
-    expect(first_new_row[:security_id]).to eq '8304 JP'
-    expect(first_new_row[:quantity_per_paramount]).to eq '200'
-    expect(first_new_row[:etfg_date]).to eq Date.new(2018,7,25)
+    Kiba.run(control)
 
-    last_new_row = bmo_holdings.process(rows.last)
-    expect(last_new_row[:ticker]).to eq 'BANK'
-    expect(last_new_row[:instrument_type]).to eq 'Equity'
-    expect(last_new_row[:name]).to eq 'BAWAG GROUP AG'
-    expect(last_new_row[:sedol]).to eq 'BZ1GZ06'
-    expect(last_new_row[:cusip]).to be_blank
-    expect(last_new_row[:security_id]).to eq 'BG AV'
-    expect(last_new_row[:quantity_per_paramount]).to eq '90'
-    expect(last_new_row[:etfg_date]).to eq Date.new(2018,7,25)
+    expect(rows.size).to eq 29
+
+    expect(rows.first[:ticker]).to eq 'BANK'
+    expect(rows.first[:instrument_type]).to eq 'Equity'
+    expect(rows.first[:name]).to eq 'AOZORA BANK LTD'
+    expect(rows.first[:sedol]).to eq 'B1G1854'
+    expect(rows.first[:cusip]).to be_blank
+    expect(rows.first[:security_id]).to eq '8304 JP'
+    expect(rows.first[:quantity_per_paramount]).to eq '200'
+    expect(rows.first[:etfg_date]).to eq Date.new(2018,7,25)
+
+    expect(rows.last[:ticker]).to eq 'BANK'
+    expect(rows.last[:instrument_type]).to eq 'Equity'
+    expect(rows.last[:name]).to eq 'BAWAG GROUP AG'
+    expect(rows.last[:sedol]).to eq 'BZ1GZ06'
+    expect(rows.last[:cusip]).to be_blank
+    expect(rows.last[:security_id]).to eq 'BG AV'
+    expect(rows.last[:quantity_per_paramount]).to eq '90'
+    expect(rows.last[:etfg_date]).to eq Date.new(2018,7,25)
   end
 end
