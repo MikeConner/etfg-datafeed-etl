60e6eac01160cb665be881ec808ddf932b5609f2
birdylockstep <bird@locksteplabs.com>
2018-08-14 15:58:57 +0700

    ETL job for Vanguard CAD All Fund Holdings

---
+++ destinations/vanguard_cad_all_fund_holdings_destination.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+class VanguardCadAllFundHoldingsDestination
+  def initialize(connect_url)
+    @conn = PG.connect(connect_url)
+    @conn.prepare('insert_pg_stmt',
+      'INSERT INTO vanguard_cad_all_fund_holdings (
+          fund_ticker, fund_name, fund_id, as_of_date, equity_holdings_count,
+          bond_holdings_count, short_term_reserve_count, listed_currency,
+          listed_currency_symbol, fund_currency, fund_currency_symbol,
+          asset_type, holding_name, ticker, cusip, sedol, isin, percent_of_fund,
+          sector, country, security_depository_receipt_type, market_value,
+          face_amount, coupon_rate, maturity_date, shares, currency_code,
+          currency_symbol, etfg_date)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15,
+        $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29);')
+  end
+
+  def write(row)
+    @conn.exec_prepared('insert_pg_stmt',
+      [
+        row[:fund_ticker],
+        row[:fund_name],
+        row[:fund_id],
+        row[:as_of_date],
+        row[:equity_holdings_count],
+        row[:bond_holdings_count],
+        row[:short_term_reserve_count],
+        row[:listed_currency],
+        row[:listed_currency_symbol],
+        row[:fund_currency],
+        row[:fund_currency_symbol],
+        row[:asset_type],
+        row[:holding_name],
+        row[:ticker],
+        row[:cusip],
+        row[:sedol],
+        row[:isin],
+        row[:percent_of_fund],
+        row[:sector],
+        row[:country],
+        row[:security_depository_receipt_type],
+        row[:market_value],
+        row[:face_amount],
+        row[:coupon_rate],
+        row[:maturity_date],
+        row[:shares],
+        row[:currency_code],
+        row[:currency_symbol],
+        row[:etfg_date]
+      ]
+    )
+  rescue PG::Error => ex
+    puts ex.message
+  end
+
+  def close
+    @conn.close
+    @conn = nil
+  end
+end
---
+++ sources/vanguard_cad_all_fund_holdings_source.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class VanguardCadAllFundHoldingsSource
+  COMMON_ROW_LABELS = [
+    'TICKER',
+    'FUND NAME',
+    'Fund Id',
+    'As of date',
+    'Equity holdings count',
+    'Bond holdings count',
+    'Short term reserve count',
+    'Listed currency',
+    'Listed currency symbol',
+    'Fund currency',
+    'Fund currency symbol'
+  ].freeze
+
+  def initialize(file)
+    @file = file
+  end
+
+  def each
+    CSV.foreach(@file) do |row|
+      next if row.blank?
+
+      # Reset fund information when find new one or footer text
+      if row[0] == 'TICKER' || row[0] =~ /^(Your|CGS)/
+        @common_row = []
+        @has_holdings_info = false
+      end
+
+      if COMMON_ROW_LABELS.include?(row[0])
+        @common_row << row[1]
+        next
+      end
+
+      if row[1] == 'Asset type' && row[2] == 'Holding name'
+        @has_holdings_info = true
+        next
+      end
+
+      if @common_row && @has_holdings_info
+        row.shift
+        yield(@common_row + row)
+      end
+    end
+  end
+end
