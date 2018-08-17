67cdabb0993a35e61cb9c574f480f96628b05124
birdylockstep <bird@locksteplabs.com>
2018-08-17 16:18:28 +0700

    Copied method from vanguad cad

--- lib/data_formatter.rb
+++ lib/data_formatter.rb
@@ -13,4 +13,15 @@ module DataFormatter
       row.transform_values! {|v| v == '-' ? nil : v }
     end
   end
+
+  # Correct date format to date object
+  def manipulate_maturity_date(maturity_date)
+    return [nil, nil] if maturity_date.blank?
+    date_start, date_end = maturity_date.split(' - ')
+    if date_end.nil?
+      return [Date.strptime(date_start, '%d-%b-%Y'),
+              Date.strptime(date_start, '%d-%b-%Y')]
+    end
+    [Date.strptime(date_start, '%d-%b-%Y'), Date.strptime(date_end, '%d-%b-%Y')]
+  end
 end
